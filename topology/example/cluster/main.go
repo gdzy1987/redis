6d4dd96198dbbf666cad17dcd6a98c333062f6b5
laik <laik.lj@me.com>
2019-07-17 11:13:18 +0800

    x

--- topology/example/cluster/main.go
+++ topology/example/cluster/main.go
@@ -3,8 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/json"
-	"log"
-	"os"
+	"fmt"
 
 	"github.com/dengzitong/redis/topology"
 )
@@ -19,8 +18,9 @@ func jsonPrettyPrint(in string) string {
 }
 
 func main() {
-	log.SetOutput(os.Stdout)
-	cluster := topology.CreateRedisCluster("",
+
+	var toplogyist topology.Topologist
+	toplogyist = topology.CreateRedisCluster("",
 		[][]string{
 			[]string{"10.1.181.241:8001", "10.1.181.241:8006"},
 			[]string{"10.1.181.241:8002", "10.1.181.241:8004"},
@@ -28,10 +28,14 @@ func main() {
 		},
 	)
 
-	stop := cluster.Run()
+	stop := toplogyist.Run()
 	defer stop()
-	top := cluster.Topology()
+	top := toplogyist.Topology()
 	if len(top) != 3 {
 		panic("expected 3 gourp")
+	}
+
+	for k, v := range top {
+		println(jsonPrettyPrint(fmt.Sprintf(`{ master:"%+v" , slave:"%+v" }`, k, v[1])))
 	}
 }
